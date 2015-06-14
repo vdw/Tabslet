4457b012406b54893c9542b6968575b067271e1c
vdw <dkrestos@gmail.com>
2015-06-14 22:41:11 +0300

    Bump up version

--- jquery.tabslet.min.js
+++ jquery.tabslet.min.js
@@ -4,7 +4,7 @@
  * @copyright Copyright 2015, Dimitris Krestos
  * @license   Apache License, Version 2.0 (http://www.opensource.org/licenses/apache2.0.php)
  * @link      http://vdw.staytuned.gr
- * @version   v1.4.8
+ * @version   v1.4.9
  */
 
   /* Sample html structure
